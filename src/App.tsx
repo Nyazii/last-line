38c4b7e8a035f5e888d5d607d52f8db9f9021406
Nyazii <marcosdpmiranda@gmail.com>
2025-08-21 20:13:02 -0300

    initial project last line

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-3xl font-bold">Hello, Tailwind!</h1>
+    </div>
+  );
+}
